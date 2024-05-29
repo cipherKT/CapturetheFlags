a5eab15a9e06e1e075525f7748f68e677b4cb8ce
cipherKT <ktcipher@gmail.com>
2024-05-29 02:45:55 -0800

    webDecode  done

--- git_auto.py
+++ git_auto.py
@@ -12,15 +12,15 @@ def question(name):
 
 def main():
     option = int(input("Choose option \n1. Enter a message \n2. Completed a question \nany other key to quit \ninput : "))
-    while(True):
-        if(option == 1):
-            message = input("Enter the commit message : ")  
-            auto(message)
-        elif(option == 2):
-            name = input("Enter the name of the question : ")
-            question(name)
-        else:
-            break
+        
+    if(option == 1):
+        message = input("Enter the commit message : ")  
+        auto(message)
+    elif(option == 2):
+        name = input("Enter the name of the question : ")
+        question(name)
+    else:
+        return 
          
 
 if __name__ == "__main__":
